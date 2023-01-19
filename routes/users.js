ea7096c33c1088ab21ee92a8d51877561ad27fdc
isaac <isaacikogan@gmail.com>
2023-01-19 16:14:55 -0500

    Add owner gif resource

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const config = require("../resources/config.json");
+const axios = require("axios");
+const path = require("path");
 
 function getOnlineUsers(req) {
     return req.app.usercache.getStats()["keys"];
@@ -14,5 +17,9 @@ router.get("/", async (req, res) => {
 
 });
 
+
+router.get("/owner/gif", async (req, res) => {
+    return res.sendFile(path.join(__dirname, "../", "/resources/owner_gif"));
+});
 
 module.exports = router;
